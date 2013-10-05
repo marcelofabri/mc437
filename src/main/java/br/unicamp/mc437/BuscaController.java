f74e013af081e4f4fb5a8795b9e0015dc02caf3f
Marcelo Fabri <marcelofabrimf@gmail.com>
2013-10-04 22:19:36 -0300

    Separando as chamadas

--- src/main/java/br/unicamp/mc437/BuscaController.java
+++ src/main/java/br/unicamp/mc437/BuscaController.java
@@ -2,26 +2,40 @@ package br.unicamp.mc437;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.unicamp.mc437.model.Patrimonio;
 
 @Controller
-@RequestMapping("/hello")
-public class TesteController {
+@RequestMapping("/")
+public class BuscaController {
  
 	@PersistenceContext(unitName = "mc437PersistenceUnit")
 	private EntityManager entityManager;
 	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "/busca", method = RequestMethod.GET)
 	@Transactional
-	public ModelAndView printHello(ModelMap model) {
+	public ModelAndView buscarPatrimonios(ModelMap model) {
+	  
+		Query query = entityManager.createQuery("SELECT p FROM Patrimonio p");
+		
+		ModelAndView mav = new ModelAndView("busca");
+	    mav.addObject("lista", query.getResultList());
+	    
+		return mav;
+	}
+	
+	@RequestMapping(value = "/importar", method = RequestMethod.GET)
+	@Transactional
+	public @ResponseBody String importarDaPlanilha(ModelMap model) {
 	  
 		ExcelXLSX excelXLSX = new ExcelXLSX();
 		excelXLSX.lerPlanilha();
@@ -31,12 +45,8 @@ public class TesteController {
 		}
 		
 		entityManager.flush();
-		
-		System.out.println("TESTETEEETETETETE");
-		ModelAndView mav = new ModelAndView("busca");
-	    mav.addObject("lista", excelXLSX.getLista());
 	    
-		return mav;
+		return "Pronto!";
 	}
 
 }
