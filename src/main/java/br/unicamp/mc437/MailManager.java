48e5ed30c68a44e15ba672e5f9ad09e1a4f17e52
Lais <lminchillo2@gmail.com>
2013-11-27 21:35:35 -0200

    Criada classe para gerenciar email

---
+++ src/main/java/br/unicamp/mc437/MailManager.java
@@ -0,0 +1,51 @@
+package br.unicamp.mc437;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class MailManager
+{
+	public MailManager()
+	{
+		final String username = "patrimonio.ic.unicamp@gmail.com";
+		final String password = "xC32eQkXArzKCpDDpDKqvHreGuXeYT";
+ 
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+ 
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+ 
+		try {
+ 
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("patrimonio.ic.unicamp@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("lminchillo2@gmail.com,marcelofabri.mf@gmail.com"));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+ 
+			Transport.send(message);
+ 
+			System.out.println("Done");
+ 
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
